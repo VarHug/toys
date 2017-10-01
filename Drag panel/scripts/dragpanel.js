1d73721dde889b56bfe8f1a7d80ae7f5e78aef65
VarHug <490641570@qq.com>
2017-10-01 11:55:01 +0800

    Finish Drag Panel

--- Drag panel/scripts/dragpanel.js
+++ Drag panel/scripts/dragpanel.js
@@ -11,18 +11,25 @@ function getByClass(className, parent) {
     }
     return eles;
 }
+function getById(classId) {
+    return document.getElementById(classId);
+}
 
-window.onload = dragPanel;
+EventUtil.addHandler(window, 'load', dragPanel);
 
 function dragPanel() {
     //用户名框判定
     inputUidJudge();
     //面板拖动
     panelMove();
+    //关闭面板
+    panelClose();
+    //切换状态
+    changeState();
 }
-
+//用户名框判定
 function inputUidJudge() {
-    let inputUid = document.getElementById('uid');
+    let inputUid = getById('uid');
     EventUtil.addHandler(inputUid, 'focus', function(event) {
         if(inputUid.value === 'QQ号码或Email帐号') {
             inputUid.value = '';
@@ -34,28 +41,102 @@ function inputUidJudge() {
         }
     });
 }
-
+//面板拖动
 function panelMove() {
     let oTitle = document.getElementsByClassName ? document.getElementsByClassName('login_logo_webqq')[0] : getByClass('login_logo_webqq', 'loginPanel')[0];
     EventUtil.addHandler(oTitle, 'mousedown', fnDown);
 
+    //按下鼠标
     function fnDown(event) {
         let eventFnDown = EventUtil.getEvent(event);
-        let oDrag = document.getElementById('loginPanel'),
+        let oDrag = getById('loginPanel'),
+            //鼠标点击时与面板左和上的距离
             disX = eventFnDown.clientX - oDrag.offsetLeft,
             disY = eventFnDown.clientY - oDrag.offsetTop;
-        EventUtil.addHandler(document, 'mousemove', function(event) {
+        //添加移动鼠标事件处理程序    
+        EventUtil.addHandler(document, 'mousemove', fnMove);
+        //添加松开鼠标事件处理程序
+        EventUtil.addHandler(document, 'mouseup', fnUp);
+        //移动鼠标事件处理程序 
+        function fnMove(event) {
             let eventMousemove = EventUtil.getEvent(event);
-            fnMove(eventMousemove, disX, disY);
-        });
-    }
-
-    function fnMove(e, posX, posY) {
-        let l = e.clientX - posX,
-            t = e.clientY - posY;
-            oDrag = document.getElementById('loginPanel');
-        oDrag.style.left = l + 'px';
-        oDrag.style.top = t + 'px';
+            fnMove1(eventMousemove, disX, disY);
+            function fnMove1(e, posX, posY) {
+                let oDrag = getById('loginPanel'),
+                    //盒子正确的位置
+                    l = e.clientX - posX,
+                    t = e.clientY - posY,
+                    winW = document.documentElement.clientWidth || document.body.clientWidth,
+                    winH = document.documentElement.clientHeight || document.body.clientHeight,
+                    maxW = winW - oDrag.offsetWidth - 10,
+                    maxH = winH - oDrag.offsetHeight;
+                
+                if (l < 0) {
+                    l = 0;
+                } else if(l > maxW) {
+                    l = maxW;
+                }
+                if(t < 10) {
+                    t = 10;
+                } else if(t > maxH) {
+                    t = maxH;
+                }
+                oDrag.style.left = l + 'px';
+                oDrag.style.top = t + 'px';
+            }
+        }
+        //松开鼠标事件处理程序
+        function fnUp(event) {
+            //移除移动鼠标事件处理程序
+            EventUtil.removerHandler(document, 'mousemove', fnMove);
+            //移除松开鼠标事件处理程序
+            // EventUtil.removerHandler(document, 'mouseup', fnUp);
+        }
     }
 }
-
+//关闭面板
+function panelClose() {
+    let boxClose = getById('ui_boxClose');
+    EventUtil.addHandler(boxClose, 'click', function(event) {
+        getById('loginPanel').style.display = 'none';
+    });
+}
+//切换状态
+function changeState() {
+    let loginState = getById('loginState'),
+        stateList = getById('loginStatePanel'),
+        stateListLi = stateList.getElementsByTagName('li'),
+        stateTxt = getById('loginStateTxt'),
+        loginStateShow = getById('loginStateShow');
+    //点击切换状态时事件处理程序    
+    EventUtil.addHandler(loginState, 'click', function(event) {
+        let loginStateEvent = EventUtil.getEvent(event);
+        EventUtil.stopProagation(loginStateEvent);
+        stateList.style.display = 'block';
+    });
+    //鼠标滑过、离开和点击状态列表事件处理程序
+    for(let i = 0, len = stateListLi.length; i < len; i++) {
+        //滑过事件处理程序
+        EventUtil.addHandler(stateListLi[i], 'mouseover', function(event) {
+            stateListLi[i].style.backgroundColor = '#567';
+        });
+        //离开事件处理程序
+        EventUtil.addHandler(stateListLi[i], 'mouseout', function(event) {
+            stateListLi[i].style.backgroundColor = '#fff';
+        });
+        //点击事件处理程序
+        EventUtil.addHandler(stateListLi[i], 'click', function(event) {
+            let stateListLiEvent = EventUtil.getEvent(event);
+            EventUtil.stopProagation(stateListLiEvent);
+            stateList.style.display = 'none';
+            let liId = stateListLi[i].id;
+            stateTxt.innerText = getByClass('stateSelect_txt', liId)[0].innerText;
+            loginStateShow.className = '';
+            loginStateShow.className = 'login_state_show ' + liId;
+        }); 
+    }
+    //其他地方点击事件处理程序（隐藏ul）
+    EventUtil.addHandler(document, 'click', function(event) {
+        stateList.style.display = 'none';
+    });
+}
