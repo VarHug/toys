969c06feaa71d16b11b1e4d7bd2aa176316ce294
VarHug <490641570@qq.com>
2017-11-25 15:32:17 +0800

    任务链实现

--- RabbitAnimation/src/animation.js
+++ RabbitAnimation/src/animation.js
@@ -1,11 +1,17 @@
 'use strict';
 
+var loadImage = require('./imageloader');
+
 //初始化状态
 const STATE_INITTAL = 0;
 //开始状态
 const STATE_START = 1;
 //停止状态
 const STATE_STOP = 2;
+//同步任务
+const TASK_SYNC = 0;
+//异步任务
+const TASK_ASYNC = 1;
 
 /**
  * 帧动画库类
@@ -24,7 +30,12 @@ function Animation() {
  * @param {any} imglist 图片数组
  */
 Animation.prototype.loadImage = function (imglist) {
-    
+    var taskFn = function (next) {
+        loadImage(imglist.slice(), next);
+    };
+    var type = TASK_SYNC;
+
+    return this._add(taskFn, type);
 };
 
 /**
@@ -72,7 +83,17 @@ Animation.prototype.then = function (callback) {
  * @param {any} interval 
  */
 Animation.prototype.start = function (interval) {
-     
+     if(this.state === STATE_START) {
+         return this;
+     }
+     //任务链中没有任务，直接返回
+     if(!this.taskQueue.length) {
+         return this;
+     }
+     this.state = STATE_START;
+     this.interval = interval;
+     this._runTask();
+     return this;
 };
 
 /**
@@ -124,3 +145,72 @@ Animation.prototype.restart = function () {
 Animation.prototype.dispose = function () {
     
 }
+
+/**
+ * 添加一个任务到任务队列中 
+ * 
+ * @param {any} taskFn 任务方法
+ * @param {any} type 任务类型
+ */
+Animation.prototype._add = function (taskFn, type) {
+    this.taskQueue.push({
+        taskFn: taskFn,
+        type: type
+    });
+
+    return this;
+};
+
+/**
+ * 执行任务
+ * 
+ */
+Animation.prototype._runTask = function () {
+    if(!this.taskQueue || this.state !== STATE_START) {
+        return;
+    }
+    //任务执行完毕
+    if(this.index === this.taskQueue.length) {
+        this.dispose();
+        return;
+    }
+    //获得任务链上的当前任务
+    var task = this.taskQueue[this.index];
+    if(task.type === TASK_SYNC) {
+        this._syncTask(task);
+    } else {
+        this._asyncTask(task);
+    }
+};
+/**
+ * 同步任务
+ * 
+ * @param {any} task 执行的任务对象
+ */
+Animation.prototype._syncTask = function (task) {
+    var that = this;
+    var next = function () {
+      //切换到下一个任务  
+      that._next();
+    };
+
+    var taskFn = task.taskFn;
+    taskFn(next);
+};
+/**
+ * 异步任务
+ * 
+ * @param {any} task 执行的任务对象
+ */
+Animation.prototype._asyncTask = function (task) {
+    
+};
+
+/**
+ * 切换到下一个任务
+ * 
+ */
+Animation.prototype._next = function () {
+    this.index++;
+    this._runTask();
+};
