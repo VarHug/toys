392ef7e223b88b1d3ec20def010d63675d0a853b
VarHug <490641570@qq.com>
2018-01-04 14:56:20 +0800

    参数配置

--- Slide_Carousel/scripts/carousel.js
+++ Slide_Carousel/scripts/carousel.js
@@ -1,13 +1,45 @@
-(function () {
+(function ($) {
     /**
      * 旋转木马类
      * 
      * @param {any} poster 旋转木马控件
      */
     var Carousel = function (poster) {
-        
+        this.$poster = poster;
+        //默认配置参数
+        this.setting = {
+            "width" : 1000,
+            "height" :  347,
+            "posterWidth" : 800,
+            "posterHeight" : 347,
+            "scale" : 0.9,
+            "speed" : 500,
+            "verticalAlign" : "middle"
+        };
+
+        $.extend(this.setting, this.getSetting());
+        console.log(this.setting);
     }
 
+    /**
+     * 获取配置参数
+     * 
+     */
+    Carousel.prototype.getSetting = function () {
+        var setting = this.$poster.data('setting');
+        if(setting && setting !== '') {
+            return setting;
+        } else {
+            return {};
+        }
+    }
+
+    
+    /**
+     * 执行旋转木马类构造函数，实例化每一个旋转木马组件
+     * 
+     * @param {any} posters 
+     */
     Carousel.init = function (posters) {
         var _this_ = this;
         posters.each(function () {
@@ -16,4 +48,4 @@
     };
 
     window['Carousel'] = Carousel;
-})();
+})(jQuery);
--- Slide_Carousel/scripts/main.js
+++ Slide_Carousel/scripts/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
+    //配置参数
+    var setting = {
+        width : 1000,
+        height :  347,
+        posterWidth : 800,
+        posterHeight : 347,
+        scale : 0.9,//比例关系
+        speed : 500,//切换速度
+        verticalAlign : 'middle' //对齐方式
+    };
+
     Carousel.init($('.poster_main'));
 });
