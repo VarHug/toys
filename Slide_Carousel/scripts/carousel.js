6589c4a737cec568ce2b421fe086d14498f1e8b6
VarHug <490641570@qq.com>
2018-01-04 17:22:10 +0800

    设置旋转木马区域属性

--- Slide_Carousel/scripts/carousel.js
+++ Slide_Carousel/scripts/carousel.js
@@ -6,19 +6,29 @@
      */
     var Carousel = function (poster) {
         this.$poster = poster;
+        this.$poster_list = poster.find('.poster_list');
+        this.$prevBtn = poster.find('.poster_prev_btn');
+        this.$nextBtn = poster.find('.poster_next_btn');
+        this.$posterItems = poster.find('.poster_item');
+        this.$posterFirstItem = this.$posterItems.eq(0);
+        this.posterCount = this.$posterItems.length;
+        
         //默认配置参数
         this.setting = {
-            "width" : 1000,
-            "height" :  347,
-            "posterWidth" : 800,
-            "posterHeight" : 347,
+            "width" : 1000,              //区域的宽度
+            "height" :  347,             //区域的高度
+            "posterWidth" : 800,         //图片的宽度
+            "posterHeight" : 347,        //图片的高度
             "scale" : 0.9,
             "speed" : 500,
             "verticalAlign" : "middle"
         };
 
+        //获取配置参数
         $.extend(this.setting, this.getSetting());
         console.log(this.setting);
+        //设置配置参数
+        this.setSettingValue();
     }
 
     /**
@@ -32,7 +42,35 @@
         } else {
             return {};
         }
-    }
+    };
+
+    /**
+     * 设置配置参数
+     * 
+     */
+    Carousel.prototype.setSettingValue = function () {
+        this.$poster.css({
+            width : this.setting.width,
+            height : this.setting.height
+        });
+        this.$poster_list.css({
+            width : this.setting.width,
+            height : this.setting.height
+        });
+        this.$prevBtn.css({
+            zIndex : Math.ceil(this.posterCount / 2)
+        });
+        this.$nextBtn.css({
+            zIndex : Math.ceil(this.posterCount / 2)
+        });
+
+        //第一帧图片的绝对定位左边距
+        var dis = (this.setting.width - this.setting.posterWidth) / 2;
+        this.$posterFirstItem.css({
+            left : dis,
+            zIndex : this.posterCount / 2 | 0
+        });
+    };
 
     
     /**
