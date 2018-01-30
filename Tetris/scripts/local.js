9c031d9337b31b9e1ed458d5abe103dcfa936691
VarHug <490641570@qq.com>
2018-01-30 15:38:26 +0800

    down
    添加下移操作

--- Tetris/scripts/local.js
+++ Tetris/scripts/local.js
@@ -1,8 +1,10 @@
 
 var Local = function () {
-    //游戏对象
-    var game;
-    
+
+    /**
+     * 开始
+     * 
+     */
     var start = function () {
         var doms = {
             gameDiv : document.getElementById('game'),
--- Tetris/scripts/tetris.js
+++ Tetris/scripts/tetris.js
@@ -35,6 +35,8 @@
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ];
+        this.gameDataRow = this.gameData.length;
+        this.gameDataCol = this.gameData[0].length;
         //当前方块
         this.cur = new Square();
         //下一个方块
@@ -43,15 +45,12 @@
         this.gameDivs = [];
         this.nextDivs = [];
 
+        this.bindKeyEvent();
         this.initDiv(this.gameDiv, this.gameData, this.gameDivs);
         this.initDiv(this.nextDiv, this.next.data, this.nextDivs);
         this.cur.origin.x = 10;
         this.cur.origin.y = 5;
-        // for(let i = 0; i < this.cur.data.length; i++) {
-        //     for(let j = 0; j < this.cur.data[0].length; j++) {
-        //         this.gameData[this.cur.origin.x + i][this.cur.origin.y + j] = this.cur.data[i][j];
-        //     }
-        // }
+        this.setData();
         this.refreshDiv(this.gameData, this.gameDivs);
         this.refreshDiv(this.next.data, this.nextDivs);
     };
@@ -76,7 +75,7 @@
             }
             divs.push(div);
         }
-    }
+    };
 
     /**
      * 刷新区域
@@ -96,7 +95,102 @@
                 }
             }
         }
+    };
+
+    /**
+     * 设置数据
+     * 
+     */
+    Tetris.prototype.setData = function () {
+        for(let i = 0; i < this.cur.data.length; i++) {
+            for(let j = 0; j < this.cur.data[0].length; j++) {
+                //if (this.check(this.cur.origin, i, j)) {
+                    this.gameData[this.cur.origin.x + i][this.cur.origin.y + j] = this.cur.data[i][j];
+                //}
+            }
+        }
+    };
+
+    /**
+     * 清除数据
+     * 
+     */
+    Tetris.prototype.clearData = function () {
+        for(let i = 0; i < this.cur.data.length; i++) {
+            for(let j = 0; j < this.cur.data[0].length; j++) {
+                //if(this.check(this.cur.origin, i, j)) {
+                    this.gameData[this.cur.origin.x + i][this.cur.origin.y + j] = NONE;
+                //}
+            }
+        }
     }
+
+    /**
+     * 边界检测
+     * 
+     * @param {object{x:number, y:number}} pos 
+     * @param {number} x 
+     * @param {number} y 
+     */
+    Tetris.prototype.check = function (pos, x, y) {
+        if (pos.x + x < 0) {  //上边界
+            return false;
+        } else if (pos.x + x >= this.gameDataRow) { //下边界
+            return false;
+        } else if (pos.y + y < 0) {  //左边界
+            return false;
+        } else if (pos.y + y >= this.gameDataCol) { //右边界
+            return false;
+        } else if (this.gameData[pos.x + x][pos.y + y] === DONE) { //碰上下落的块
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * 绑定键盘事件
+     * 
+     */
+    Tetris.prototype.bindKeyEvent = function () {
+        var that = this;
+        document.onkeydown = function (e) {
+            if (e.keyCode === 38) { //up
+
+            } else if (e.keyCode === 39) { //right
+
+            } else if (e.keyCode === 40) { //down
+                that.down();
+            } else if (e.keyCode === 37) { //left
+                
+            } else if (e.keyCode === 32) { //space
+                
+            }
+        };
+    };
+
+    /**
+     * 下移
+     * 
+     */
+    Tetris.prototype.down = function () {
+        var test = {
+            x : this.cur.origin.x + 1,
+            y : this.cur.origin.y
+        };
+        for (let i = 0, dataRow = this.cur.data.length; i < dataRow; i++) {
+            for (let j = 0, dataCol = this.cur.data[0].length; j < dataCol; j++) {
+                if(!this.check(test, i, j)) {
+                    return;
+                }
+            }
+        }
+        this.clearData();
+        this.cur.origin.x += 1;
+        this.setData();
+        this.refreshDiv(this.gameData, this.gameDivs);
+    };
+
     /**
      * 初始化
      * 
@@ -104,7 +198,8 @@
      */
     Tetris.init = function (doms) {
         new this(doms);
-    }
+    };
+
 
     window['Tetris'] = Tetris;
 })();
