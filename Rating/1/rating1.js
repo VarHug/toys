52c953c8a60b5939399168c8a054424cc60a52d1
VarHug <490641570@qq.com>
2018-03-13 15:17:52 +0800

    抽象父类

--- Rating/1/rating1.js
+++ Rating/1/rating1.js
@@ -1,19 +1,29 @@
 $(document).ready(function () {
 
 var rating = (function () {
-    //点亮整颗
-    var LightEntire = function (ele, options) {
+    //继承方法
+    var extend = function (subClass, superClass) {
+        var F = function () {};
+        F.prototype = superClass.prototype;
+        subClass.prototype = new F();
+        subClass.prototype.construtor = subClass;
+    };
+
+    //点亮父类
+    var Light = function (ele, options) {
         this.$ele = $(ele);
         this.$item = this.$ele.find('.rating-item');
         this.opts = options;
+        this.add = 1;
+        this.selectEvent = 'mouseover';
     };
-    LightEntire.prototype.init = function () {
+    Light.prototype.init = function () {
         this.lightOn(this.opts.num);
         if (!this.opts.readOnly) {
             this.bindEvents();
         }  
     };
-    LightEntire.prototype.lightOn = function (num) {
+    Light.prototype.lightOn = function (num) {
         num = parseInt(num);
         this.$item.each(function (index) {
             if (index < num) {
@@ -27,72 +37,15 @@ var rating = (function () {
             }
         });
     };
-    LightEntire.prototype.bindEvents = function () {
+    Light.prototype.bindEvents = function () {
         var _this_ = this,
             itemLength = _this_.$item.length;
-        _this_.$ele.on('mouseover', '.rating-item', function () {
-            var num = $(this).index() + 1;
-            _this_.lightOn(num);
-            
-            (typeof _this_.opts.select === 'function') && _this_.opts.select.call(this, num, itemLength);
-            _this_.$ele.trigger('select', [num, itemLength]);
-        }).on('click', '.rating-item', function () {
-            _this_.opts.num = $(this).index() + 1;
-
-            (typeof _this_.opts.chosen === 'function') && _this_.opts.chosen.call(this, _this_.opts.num, itemLength);
-            _this_.$ele.trigger('chosen', [_this_.opts.num, itemLength]);
-        }).on('mouseout', function () {
-            _this_.lightOn(_this_.opts.num); 
-        }); 
-    };
-
-    //点亮半颗
-    var LightHalf = function (ele, options) {
-        this.$ele = $(ele);
-        this.$item = this.$ele.find('.rating-item');
-        this.opts = options;
-        this.add = 1;
-    };
-    LightHalf.prototype.init = function () {
-        this.lightOn(this.opts.num);
-        if (!this.opts.readOnly) {
-            this.bindEvents();
-        }  
-    };
-    LightHalf.prototype.lightOn = function (num) {
-        var count = parseInt(num),
-            isHalf = count !== num;
-        this.$item.each(function (index) {
-            if (index < num) {
-                $(this).css({
-                    'background-position' : '-1px -40px'
-                });
-            } else {
-                $(this).css({
-                    'background-position' : '0 0'
-                });
-            }
-        });
-
-        if (isHalf) {
-            this.$item.eq(count).css({
-                'background-position' : '0 -80px'
-            });
-        }
-    };
-    LightHalf.prototype.bindEvents = function () {
-        var _this_ = this,
-            itemLength = _this_.$item.length;
-
-        _this_.$ele.on('mousemove', '.rating-item', function (e) {
+        _this_.$ele.on(_this_.selectEvent, '.rating-item', function (e) {
             var $this = $(this),
                 num = 0;
-            //半颗判断
-            if (e.pageX - $this.offset().left < $this.width() / 2) {
-                _this_.add = 0.5;
-            } else {
-                _this_.add = 1;
-            }
+
+            _this_.select(e, $this);
+
             num = $this.index() + _this_.add;
             _this_.lightOn(num);
             
@@ -107,9 +60,51 @@ var rating = (function () {
             _this_.lightOn(_this_.opts.num); 
         }); 
     };
+    Light.prototype.select = function () {
+        throw new Error('子类必须重写该方法');
+    };
+
+    //点亮整颗
+    var LightEntire = function (ele, options) {
+        Light.call(this, ele, options);
+        this.selectEvent = 'mouseover';
+    };
+    extend(LightEntire, Light);
+    LightEntire.prototype.lightOn = function (num) {
+        Light.prototype.lightOn.call(this, num);
+    };
+    LightEntire.prototype.select = function () {
+        this.add = 1;  
+    };
+
+    //点亮半颗
+    var LightHalf = function (ele, options) {
+        Light.call(this, ele, options);
+        this.selectEvent = 'mousemove';
+    };
+    extend(LightHalf, Light);
+    LightHalf.prototype.lightOn = function (num) {
+        var count = parseInt(num),
+            isHalf = count !== num;
+
+        Light.prototype.lightOn.call(this, count);
+
+        if (isHalf) {
+            this.$item.eq(count).css({
+                'background-position' : '0 -80px'
+            });
+        }
+    };
+    LightHalf.prototype.select = function (e, $this) {
+        if (e.pageX - $this.offset().left < $this.width() / 2) {
+            this.add = 0.5;
+        } else {
+            this.add = 1;
+        }
+    };
 
     var defaults = {
-        mode : 'LightHalf',
+        mode : 'LightEntire',
         num : 0,
         readOnly : false,
         select : function () {},
@@ -138,17 +133,18 @@ var rating = (function () {
 })();
 
 rating.init('#rating', {
-    num : 2.5,
+    mode : 'LightEntire',
+    num : 2.5
     // select: function (num, total) {
     //     console.log(this);
     //     console.log(num + '/' + total);
     // }
 });
-$('#rating').on('select', function (e, num, total) {
-    console.log(num + '/' + total);
-}).on('chosen', function (e, num, total) {
-    console.log(num + '/' + total);
-});
+// $('#rating').on('select', function (e, num, total) {
+//     console.log(num + '/' + total);
+// }).on('chosen', function (e, num, total) {
+//     console.log(num + '/' + total);
+// });
 
 
 
