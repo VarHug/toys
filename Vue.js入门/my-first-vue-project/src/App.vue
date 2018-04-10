bf487b21866991b9d5dab6a306777686e8552ede
VarHug <490641570@qq.com>
2018-04-10 14:42:18 +0800

    Add todolist1

--- Vue.js入门/my-first-vue-project/src/App.vue
+++ Vue.js入门/my-first-vue-project/src/App.vue
@@ -1,13 +1,38 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <img src="./assets/logo.png">
+    <router-view/> -->
+    <h1>{{title}}</h1>
+    <h1 v-text="title2"></h1>
+    <h1 v-html="title3"></h1>
+    <ul>
+      <li v-bind:class="{finished: item.isFinished}" v-for="item in items">
+        {{item.label}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  // name: 'App'
+  data() {
+    return {
+      title : 'this is a todo list',
+      title2 : 'this is a title2 test',
+      title3 : '<p>this is a v-html test</p>',
+      items : [
+        {
+          label: 'coding',
+          isFinished: false
+        },
+        {
+          label: 'walking',
+          isFinished: true
+        }
+      ],
+    }
+  }
 }
 </script>
 
@@ -19,5 +44,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.finished {
+  text-decoration: underline;
 }
 </style>
