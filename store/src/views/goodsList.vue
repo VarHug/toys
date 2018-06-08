642594041a71d867f5bb804e351a4bb35ca5a90f
VarHug <490641570@qq.com>
2018-06-08 13:53:36 +0800

    价格排序

--- store/src/views/goodsList.vue
+++ store/src/views/goodsList.vue
@@ -7,7 +7,7 @@
         <div class="filter-nav">
           <span class="sortby">Sort by:</span>
           <a href="javascript:void(0)" class="default cur">Default</a>
-          <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+          <a href="javascript:void(0)" class="price" @click="sortGoods">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
           <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
         </div>
         <div class="accessory-result">
@@ -32,7 +32,7 @@
                   </div>
                   <div class="main">
                     <div class="name">{{item.productName}}</div>
-                    <div class="price">{{item.prodcutPrice}}</div>
+                    <div class="price">{{item.salePrice}}</div>
                     <div class="btn-area">
                       <a href="javascript:;" class="btn btn--m">加入购物车</a>
                     </div>
@@ -54,6 +54,8 @@ import vHeader from '../components/header/header';
 import vFooter from '../components/footer/footer';
 import vBread from '../components/bread/bread';
 
+const ERR_OK = 0;
+
 export default {
   data() {
     return {
@@ -72,14 +74,14 @@ export default {
       ],
       priceChecked: 'all',
       filterBy: false,
-      overLayFlag: false
+      overLayFlag: false,
+      sortFlag: true,
+      page: 1,
+      pageSize: 8
     };
   },
   mounted() {
-    this.$axios.get('/goods').then(response => {
-      var res = response.data;
-      this.goodsList = res.result.list;
-    });
+    this.getGoodsList();
   },
   methods: {
     setPriceFilter(index) {
@@ -93,6 +95,28 @@ export default {
     closePop() {
       this.filterBy = false;
       this.overLayFlag = false;
+    },
+    getGoodsList() {
+      var param = {
+        page: this.page,
+        pageSize: this.pageSize,
+        sort: this.sortFlag ? 1 : -1
+      };
+      this.$axios.get('/goods', {
+        params: param
+      }).then(response => {
+        let res = response.data;
+        if (res.status === ERR_OK) {
+          this.goodsList = res.result.list;
+        } else {
+          this.goodsList = [];
+        }
+      });
+    },
+    sortGoods() {
+      this.sortFlag = !this.sortFlag;
+      this.page = 1;
+      this.getGoodsList();
     }
   },
   components: {
